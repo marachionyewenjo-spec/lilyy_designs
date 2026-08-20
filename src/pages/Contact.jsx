127cce793dcb43cd39500bce634e8eeb7513b3f7
marachionyewenjo-spec <marachionyewenjo@gmail.com>
2026-08-20 10:58:10 +0100

    few updates

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,12 +1,12 @@
 import { useState } from "react";
 import api from "../api/client";
-import {FaFacebook, FaGithub, FaInstagram, FaLinkedin, FaTiktok, FaWhatsapp} from "react-icons/fa";
+import {FaFacebook, FaGithub, FaInstagram, FaLinkedin, FaPinterest, FaTiktok, FaWhatsapp} from "react-icons/fa";
 
 export default function Contact() {
 
     return (
         <section className="bg-[radial-gradient(circle_at_top,_#fff7ed_0%,_#f8fafc_55%,_#f1f5f9_100%)] py-20 ">
-            <div className="w-[400px] lg:w-[800px] md:w-[600px] mx-auto">
+            <div className="w-[300px] lg:w-[800px] md:w-[600px] mx-auto">
                 <div className="grid gap-10 rounded-[32px] bg-white p-8 shadow-[0_20px_60px_rgba(15,23,42,0.08)] md:p-12">
                     <div>
                         <p className="inline-flex rounded-full bg-amber-200 px-4 py-2 text-sm font-semibold uppercase tracking-[0.25em] text-amber-800">
@@ -39,17 +39,17 @@ export default function Contact() {
                     <div>
                         <div className="flex gap-5 items-center justify-center text-2xl text-slate-600">
 
-                            <a href=""> <FaFacebook/> </a>
+                            <a href="https://www.facebook.com/lilian.alexander.902"> <FaFacebook/> </a>
 
-                            <a href=""> <FaLinkedin/> </a>
+                            <a href="https://www.linkedin.com/in/lilian-alexander-382b8836b?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"> <FaLinkedin/> </a>
 
-                            <a href=""> <FaTiktok/> </a>
+                            <a href="https://www.tiktok.com/@lilyy_designs?_t=ZS-90doGtEr1Vg&_r=1"> <FaTiktok/> </a>
 
-                            <a href=""> <FaInstagram/> </a>                                                 
+                            <a href="https://www.instagram.com/lilyy_designz?igsh=cnJ2NTZlbXN6eXVy"> <FaInstagram/> </a>                                                 
 
-                            <a href="" > <FaGithub/> </a>
+                            <a href="https://pin.it/725PqQVSS" > <FaPinterest/> </a>
 
-                            <a href=""> <FaWhatsapp/> </a>
+                            <a href="https://wa.me/2347030523750"> <FaWhatsapp/> </a>
                         </div>
                     </div>
                 </div>
